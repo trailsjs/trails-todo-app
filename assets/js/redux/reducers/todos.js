ac68e21ce4210020fbf92a8a3d0f877323e0b2da
Blaine Price <1wbprice@gmail.com>
2016-05-24 14:35:48 -0400

    Write some helper functions for managing todos.

--- assets/js/redux/reducers/todos.js
+++ assets/js/redux/reducers/todos.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 import {
   FETCH_TODOS_SUCCESS,
   ADD_TODO_REQUEST,
@@ -7,10 +9,38 @@ import {
   TOGGLE_TODO_COMPLETE_SUCCESS,
   TOGGLE_TODO_EDITING,
   UPDATE_TODO_DESCRIPTION_REQUEST,
-  REMOVE_COMPLETED_TODOS_REQUEST
+  REMOVE_COMPLETED_TODOS_REQUEST,
+  TOGGLE_TODOS_COMPLETION_REQUEST,
+  TOGGLE_TODOS_COMPLETION_SUCCESS
 } from './../actions'
 
+function mergeUpdates(collection, updates) {
+
+  if (!updates.length) {
+    updates = [updates]
+  }
+
+  return collection.map(todo => {
+    const updatedTodo = updates.find(update => update.id === todo.id)
+    if (updatedTodo) {
+      todo = updatedTodo
+    }
+    return todo
+  })
+}
+
+function modifyTodo(state, todoId, callback) {
+  return state.map(todo => {
+    if (todo.id === todoId) {
+      callback(todo)
+    }
+    return todo
+  })
+}
+
 const todos = (state = [], action) => {
+
+  let uncompletedTodos = []
 
   switch (action.type) {
 
@@ -28,12 +58,7 @@ const todos = (state = [], action) => {
     ]
 
   case ADD_TODO_SUCCESS:
-    return state.map(todo => {
-      if (todo.description === action.description) {
-        todo = action.response
-      }
-      return todo
-    })
+    return mergeUpdates(state, action.response)
 
   case REMOVE_TODO_REQUEST:
     return state.filter(todo => {
@@ -41,42 +66,52 @@ const todos = (state = [], action) => {
     })
 
   case TOGGLE_TODO_EDITING:
-    return state.map(todo => {
-      if (todo.id === action.id) {
-        todo.editing = !todo.editing
-      }
-      return todo
+    return modifyTodo(state, action.id, todo => {
+      todo.editing = !todo.editing
     })
 
   case TOGGLE_TODO_COMPLETE_REQUEST:
-    return state.map(todo => {
-      if (todo.id === action.id) {
-        todo.completed = !todo.completed
-      }
-      return todo
+    return modifyTodo(state, action.id, todo => {
+      todo.completed = !todo.completed
     })
 
   case TOGGLE_TODO_COMPLETE_SUCCESS:
-    return state.map(todo => {
-      if (todo.id === action.id) {
-        todo = action.response
-      }
-      return todo
-    })
+    return mergeUpdates(state, action.response)
 
   case UPDATE_TODO_DESCRIPTION_REQUEST:
-    return state.map(todo => {
-      if (todo.id === action.id) {
-        todo.description = action.description
-        todo.editing = false
-      }
-      return todo
+    return modifyTodo(state, action.id, todo => {
+      todo.description = action.description
+      todo.editing = false
     })
 
   case REMOVE_COMPLETED_TODOS_REQUEST:
     return state.filter(todo => {
       return !todo.completed
     })
+
+  case TOGGLE_TODOS_COMPLETION_REQUEST:
+
+    // Find all the todos with { completed: false }
+    uncompletedTodos = state.filter(todo => {
+      return !todo.completed
+    })
+
+    // If all existing todos are completed, set completion for all todos to false
+    if (!uncompletedTodos.length) {
+      return state.map(todo => {
+        todo.completed = false
+        return todo
+      })
+    }
+    else {
+      return state.map(todo => {
+        todo.completed = true
+        return todo
+      })
+    }
+
+  case TOGGLE_TODOS_COMPLETION_SUCCESS:
+    return mergeUpdates(state, action.response)
 
   default:
     return state
